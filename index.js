81a7d7820a0e3f7f1a34ea7a77e760fd52c0db32
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-04-18 17:00:35 +0200

    #20 guess and provide way to specify public path of bundles

--- index.js
+++ index.js
@@ -4,6 +4,23 @@
  * Licensed under the new BSD license, available at http://caleydo.org/license
  **************************************************************************** */
 
+Object.defineProperty(module.exports, '__publicPath', {
+	set: function(value) {
+		__webpack_public_path__ = value;
+	},
+	get: function() {
+		return __webpack_public_path__;
+	}
+});
+
+var parentScript = document.querySelector('script[src$="phovea_bundle_lib.js"]');
+if (parentScript) {
+  var src = parentScript.src.substring(0, parentScript.src.length - 'phovea_bundle_lib.js'.length);
+  if (src !== '.') {
+    __webpack_public_path__ = src;
+  }
+}
+
 //request index to avoid using the pre build version
 exports.core = require('phovea_core/index');
 exports.d3 = require('phovea_d3/index');
